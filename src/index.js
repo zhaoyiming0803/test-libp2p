112957749bd2fa7cb6a5b034bdbd39591ae5b254
zhaoyiming <zhaoyiming@authing.cn>
2021-12-19 22:42:02 +0800

    test ping

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const TCP = require('libp2p-tcp')
 const { NOISE } = require('libp2p-noise')
 const MPLEX = require('libp2p-mplex')
 
-const multiaddr = require('multiaddr')
+const { multiaddr } = require('multiaddr')
+
+const process = require('process')
 
 const main = async () => {
   const node = await Libp2p.create({
@@ -17,20 +19,38 @@ const main = async () => {
       streamMuxer: [MPLEX]
     }
   })
-
+  
   // start libp2p
   await node.start()
   console.log('libp2p has started')
-
+  
   // print out listening addresses
   console.log('listening on addresses:')
   node.multiaddrs.forEach(addr => {
     console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`)
   })
-
-  // stop libp2p
-  await node.stop()
-  console.log('libp2p has stopped')
+  
+  // ping peer if received multiaddr
+  console.log('process.argv: ', process.argv)
+  if (process.argv.length >= 3) {
+    const ma = multiaddr(process.argv[2])
+    console.log('ma: ', ma)
+    console.log(`pinging remote peer at ${process.argv[2]}`)
+    const latency = await node.ping(ma)
+    console.log(`pinged ${process.argv[2]} in ${latency}ms`)
+  } else {
+    console.log('no remote peer address given, skipping ping')
+  }
+  
+  const stop = async () => {
+    // stop libp2p
+    await node.stop()
+    console.log('libp2p has stopped')
+    process.exit(0)
+  }
+  
+  process.on('SIGTERM', stop)
+  process.on('SIGINT', stop)
 }
 
 main().catch((e) => {
